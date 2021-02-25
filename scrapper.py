5e0406312ccb3f341305ba1b234f02bc384c25db
Gonzalo Cerrón <gonzalojcg12@gmail.com>
2021-02-24 20:30:52 -0500

    Agregue mi proyecto

---
+++ scrapper.py
@@ -0,0 +1,65 @@
+import requests
+import lxml.html as html
+import os 
+import datetime
+
+HOME_URL = 'https://www.larepublica.co/'
+XPATH_LINK_TO_ARTICLE = '//h2/a/@href'
+XPATH_TITLE = '//div[@class = "mb-auto"]/text-fill/span/text()'
+XPATH_SUMMARY = '//div[@class = "lead"]/p/text()'
+XPATH_BODY = '//div[@class = "html-content"]/p[not(@class)]/text()'
+
+
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\'','')
+                sumary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(sumary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+        else:
+            raise ValueError (f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
+        
+
+def parse_home():
+    try:
+        response = requests.get(HOME_URL)
+        if response.status_code == 200:
+            home = response.content.decode('utf-8')
+            parsed = html.fromstring(home)
+            link_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
+
+            today = datetime.date.today().strftime('%d-%m-%y')
+
+            if not os.path.isdir(today):
+                os.mkdir(today)
+            for link in link_to_notices:
+                parse_notice(link, today)
+        else:
+            raise ValueError(f'Error: response.status_code')
+    except ValueError as ve:
+        print(ve)
+
+def run():
+    parse_home()
+
+if __name__ == '__main__':
+    run()
